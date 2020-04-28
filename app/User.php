4159b2733ead9f51446456bff3e8c62acf1cfa43
sanjay <sanjaybmodha32@gmail.com>
2020-04-28 18:37:54 +0530

    prepupulate users and questions tables using model factories and seed

--- app/User.php
+++ app/User.php
@@ -28,12 +28,10 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function questions()
+    {
+        return $this->hasmany(Questions::class);
+    }
+
+    
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+       factory(App\User::class, 3)->create()->each(function($u){
+
+        $u->question()
+          ->saveMany(
+                factory(App\Question::class, rand(1, 5))->make()
+          );
+       });
+       
     }
 }
